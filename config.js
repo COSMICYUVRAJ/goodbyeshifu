a9393c008fbb054491453e7baa0fad6247c50634
COSMICYUVRAJ <69956300+COSMICYUVRAJ@users.noreply.github.com>
2023-05-14 12:04:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,65 +1,42 @@
-import { BsFacebook, BsYoutube } from "react-icons/bs";
-import { CgCrown } from "react-icons/cg";
-import { FaDiscord } from "react-icons/fa";
-import { MdAddModerator } from "react-icons/md";
-import { SiProbot } from "react-icons/si";
-import { TbMessage2Code } from "react-icons/tb";
-const config = {
-  name: "ProBot",
-  //BotName
-  inviteLink:
-    "https://discord.com/oauth2/authorize?client_id=879602399067660338&permissions=8&scope=bot&redirect_uri=https://docs.markos.ga&response_type=code",
-  //BotInvite Link
-  Features: [
-    {
-      name: "Moderation",
-      description:
-        "Moderation is the process of eliminating or lessening extremes.",
-      logo: <MdAddModerator className="h-10 w-10" />,
-    },
-    {
-      name: "99% Uptime",
-      description:
-        "Uptime is a measure of system reliability, expressed as the percentage of time a machine, typically a computer.",
-      logo: <SiProbot className="h-10 w-10" />,
-    },
-    {
-      name: "Automation",
-      description:
-        "Automation describes a wide range of technologies that reduce  human intervention in processes.",
-      logo: <TbMessage2Code className="h-10 w-10" />,
-    },
-    {
-      name: "Welcomer",
-      description:
-        "Welcomer is the person who greet the new person in an unique and great way",
-      logo: <CgCrown className="h-10 w-10" />,
-    },
-    // You can added more features if need add
-  ],
-  //Bot Features
-  serverCount: 300, //-  - Your
-  memberCount: 700, //--   - Bot
-  executedCommand: 27000, //--  - Stats
-  availableCommand: 400, //---   - Here
-  //Do not change if you don't know about them
-  faceBookLogo: (
-    <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
-  ),
-  discordLogo: (
-    <FaDiscord className="h-10 w-10 text-[#738adb] hover:scale-125 easeIn cursor-pointer" />
-  ),
-  youTubeLogo: (
-    <BsYoutube className="h-10 w-10 text-[#FF0000] hover:scale-125 easeIn cursor-pointer" />
-  ),
-  //----------------------------------------------------------------
-  // Your social handle links
-  githubLink: "https://github.com/mohitmimani",
-  twitterLink: "https://twitter.com",
-  mediumLink: "https://medium.com/",
-  facebookLink: "https://facebook.com",
-  discordLink: "https://discord.com",
-  youtubeLink: "https://www.youtube.com/channel/UCfhRHeex9lU0ZAVJot6Z2ZQ",
-};
-
-export default config;
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Goodbye - I Will Love You Forever</title>
+  <style>
+    /* Add any custom styles here */
+  </style>
+</head>
+<body>
+  <h1>Goodbye - I Will Love You Forever</h1>
+  <p>
+    Hey Shifa,<br>
+    It was a great journey with you. I wanted and still want to continue it, but things never go as planned, right? This stupid, accursed world. I'm very sad to lose you, but I'm happy that you're happy even without me, like in the first place. I just came to make you happy, and I believe I did my job. But it seems like my role is over, and I have to go now.
+  </p>
+  <p>
+    Also shifa Like we both did alot of mistakes in this relationship and ive forgiven u for every single fault of urs and if u havent i just wanted to say i am really sorry for whatever i did made u hurt by any chance sorry k? hope you forgive me also you know mummy keeps asking about you its hard to lie everytime but i am thinking of telling her the truth she will scold me but in the end she will realise too and yeah im sorry k????? forgive me if u havent if u alr did thank you very much i appreciate it also u know your name matches ur personality u always made me smile so if like somebody in future asks me about you i will always tell them you were my healer ( which u really are) 
+  </p>
+  <p>
+    Shifa like this journey was so awesome like the memories we had together the fun we had all of it was so good and i will always remember them and i will always keep you in my heart and i can understand how you are feeling right now because im feeling the same parting ways isnt easy but fate wants us to part ways :( you know like always daily i kept thinking about you like before sleeping i always thought about you and everyday in the morning the first thought i used to get was of yours always!! like 12 sep 2021 man this date was super awesome and btw we met for the first time on 27 september 2020 ( i never told you but i found it using some mazik ) and like i remember every single date of special things but look at this now it doesnt even matters man i fr miss you so much   </p>
+  <p>
+    I really cared about you a lot, and I still do. But you know, in this generation, only 2% of teens succeed in continuing as lovers. I always thought we were within those 2%, but now we're counted in the 98%. I really wished we could get back together, you know? It's really hard to sleep at night; I always miss the old days when we used to be so good. But anyways, it's bye-bye time.
+  </p>
+  <p>
+    I don't think there's any future ahead of us because I know you can control yourself. You're matured, and you won't get overwhelmed by love. I even sensed this days ago that if we broke up, you wouldn't come back to me. But hey, don't think of this in a negative way; I'm just telling you, okay? You stupid bAKa. And yeah, I will continue to love you &lt;3. Even if you come back to me, I don't trust myself that I will treat you right, so I won't accept you for your sake. But yeah, there is absolutely no future for us now, I guess. But it's fine because you want to live free, right? So live your life to its fullest without any limits and in your own way. 
+  </p>
+  <p>
+    Shifa you got really big potential ik who you are and what you're capable of i want you to succeed in life and also make sure to achieve all ur goals and be happy i will be always looking after you i will always support you sadly not as a lover but as a fan shifa hope you make ur parents proud and be happy and study hard for your future And I will too because i dont want you to worry about me you should worry about yourself youve gotten really skinny uk pls eat alot u should take care of yourself like its you "YOU" you have to care for urself to do other stuff YEAH? GET IT DUMBASS?? yeah then find success I will always pray for you Shifa and will always LOVE YOU Be happy and by the time im writing this its really painfull to say goodbye to you and it hurts alot than you think like i regret making this decision but for our future it was required i knew what was happening to us and it was required so we can focus on our future first maybe if fate wants us back we will get together i dont want to force anyone to love me i want them to love me by the way i am not force love bruh alsooo shifa as ive told you like the secrets we hold that NOBODY knows lets make sure to hold them right? pls? yeah and i will make u feel proud too i wont dissapoint you lets find success together separately ( for now ) 
+ </p>
+  <p>   
+    And yeah, I made this website as a memory. I hope you can remember me through this website. It's a virtual memory for you that can be accessed from anywhere, on any device.
+</p>
+  <p>
+    Well then Bye Bye Love you from bottom of my heart
+  </p>
+  <h1>Only Yours - Yuvraj(uz)</h1> 
+  </p>
+  <h1> bye bye</h1>
+  <!-- Add your images here -->
+  <img src="https://media.istockphoto.com/id/1353763028/vector/handwritten-lettering-of-wishing-you-all-the-best-vector-illustration.jpg?s=612x612&w=0&k=20&c=8GSkuZ3A2JGHSIo0hPa0ur1kbG1ewV4JBhMdQbITtrM=" alt="Image 1"> <img src="https://as2.ftcdn.net/v2/jpg/05/11/19/59/1000_F_511195967_XOUqu91sOdlte5pNoR8poBFCL1itz4RD.jpg" alt="Image 2">
+  
+</body>
+</html>
